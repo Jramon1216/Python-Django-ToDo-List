fd0b12fbc91f5fb2fb3df58cf7df1d87069048cf
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-06-03 19:13:51 -0400

    - Added app to INSTALLED_APPS - Made migrations

--- base/models.py
+++ base/models.py
@@ -9,9 +9,8 @@ class Task(models.Model):
     complete = models.BooleanField(default=False)
     created =  models.DateTimeField(auto_now_add=True)
 
-    def __str__(self)
+    def __str__(self):
         return self.title
     
     class Meta:
         ordering = ['complete']
-        
