8a75f7ccd2e7a4ce6789446677dde760eeaaf0f2
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-06-13 19:14:40 -0400

    - Added user login and logout views

--- base/views.py
+++ base/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.contrib.auth.views import LoginView
+
 from django.urls import reverse_lazy
 
 from .models import Task
+
+class Login(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     model = Task
