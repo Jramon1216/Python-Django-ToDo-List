50df678ff7ee1b619d76abe79bc6c2e4baf833ad
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-06-17 14:05:52 -0400

    - Added user registration template and functionality - Added register path to urls - Added Register view - Removed sqlite cache - Added sqlite file to gitignore

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, Login
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, Login, Register
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', Login.as_view(),  name='login'),
     path('logout/', LogoutView.as_view(next_page='login'),  name='logout'),
+    path('register/', Register.as_view(), name='register'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
--- base/views.py
+++ base/views.py
@@ -1,13 +1,15 @@
 from django.forms import BaseModelForm
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from django.urls import reverse_lazy
 
@@ -20,6 +22,23 @@ class Login(LoginView):
     
     def get_success_url(self):
         return reverse_lazy('tasks')
+    
+class Register(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(Register, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(Register, self).get(*args, **kwargs)
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
