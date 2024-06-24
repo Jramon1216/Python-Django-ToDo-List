0276ef73a004d622eb3ab2c7164483920ef8c6e3
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-06-24 19:48:55 -0400

    - Added main.html file

--- base/views.py
+++ base/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.forms import BaseModelForm
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
@@ -47,6 +49,7 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
+        # context['tasks_json'] = json.dumps(list(Task.objects.filter(user=self.request.user).values('user', 'title', 'description', 'complete')))
         context['count'] = context['tasks'].filter(complete=False).count()
         
         search_input = self.request.GET.get('search-area') or ''
