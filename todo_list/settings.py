16c91f9caa7a740c23c3da57833cbba5634e0fb2
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-06-27 15:28:54 -0400

    - Changed path for static files in project settings - Added divs to task list template - Removed view option for tasks

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -120,7 +120,7 @@ LOGIN_URL="login"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'base/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
