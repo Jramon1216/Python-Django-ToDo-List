75f856829905a3c719c7e68c85a6ebe0847edcbe
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-10-03 14:56:07 -0400

    - Removed django debug toolbar

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'livereload',
     'django.contrib.staticfiles',
     'dj_svg',
-    'debug_toolbar',
     'base',
 ]
 
@@ -50,7 +49,6 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
